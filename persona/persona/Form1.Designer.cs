a8ce82fd3729a9d894aa2070e683533b6954adb9
Isaac1614 <id22141@dasc.edu.mx>
2024-06-06 09:47:07 -0600

    Proyecto finalizado

--- persona/persona/Form1.Designer.cs
+++ persona/persona/Form1.Designer.cs
@@ -358,7 +358,7 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(53, 44);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(153, 184);
+            this.pictureBox1.Size = new System.Drawing.Size(171, 190);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
